7328b658eb8a3df941974ece7cdac6d4e310c273
Kay <kehiiiiya@gmail.com>
2023-11-24 11:35:22 +0000

    feat: readme, syncer config

---
+++ README.md
@@ -0,0 +1,24 @@
+# NOS2ync
+
+Nostr sync is a tool that helps you to sync your last `n` tweets with your Nostr account.
+Generally it helps you to use Nostr without losing your tweets.
+
+## Install & Usage
+
+Install:
+```
+cargo install nos2ync
+```
+
+Usage:
+```
+nos2ync ./nos2ync_config.toml
+```
+
+## Contribution
+
+Contributions are welcomed!
+
+## License
+
+Nos2ync is under [MIT](./LICENSE) license
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use serde::Deserialize;
+use std::env;
+use toml::from_str;
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = args[0].clone();
+    let config = load_config_from_file(&path);
+    
+    println!("private key: {}", config.nostr_private_key);
+    println!("last n tweets: {}", config.last_n_tweets);
+    println!("twitter username: {}", config.twitter_username);
+    println!("consumer key: {}", config.consumer_key);
+    println!("consumer secret: {}", config.consumer_secret);
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    nostr_private_key: String,
+    last_n_tweets: u16,
+    twitter_username: String,
+    consumer_key: String,
+    consumer_secret: String,
+}
+
+fn load_config_from_file(path: &str) -> Config {
+    let contents = std::fs::read_to_string(path).unwrap();
+    from_str(&contents).unwrap()
+}
